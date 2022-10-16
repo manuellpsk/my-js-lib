0690aded32b4d6249b9786c6e17497d9088f4bb4
Manuel Home Ubuntu <manuellpsk@gmail.com>
2022-10-16 16:20:35 -0300

    start my-js-lib

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {Date} dateA 
+ * @param {Date} dateB 
+ */
+function differenceBetweenTwoDates(dateA, dateB) {
+    if (!(dateA instanceof Date) || !(dateB instanceof Date)) {
+        return
+    }
+    return Math.abs(dateA.getTime() - dateB.getTime())
+}
+
+export default {
+    differenceBetweenTwoDates
+}
---
+++ test.js
@@ -0,0 +1,5 @@
+import { differenceBetweenTwoDates } from './index.js';
+
+const dateA = new Date(2022, 10, 15)
+const dateB = new Date(2022, 10, 16)
+console.log(differenceBetweenTwoDates(dateA, dateB))
