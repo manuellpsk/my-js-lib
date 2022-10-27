1c1f16f5cf3346b9d7eb4524242637aa8b70a1e6
Manolete Lopez <manuel@MacBook-Pro-de-Manuel.local>
2022-10-27 00:55:47 -0300

    add round function

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function differenceBetweenTwoDates(dateA, dateB) {
     return Math.abs(dateA.getTime() - dateB.getTime())
 }
 
+
+/**
+ * 
+ * @param {number} number `
+ * @param {number} digits 
+ */
+function round(num, digits) {
+    return +(Math.round(num + `e+${digits}`) + `e-${digits}`);
+}
+
 export {
-    differenceBetweenTwoDates
+    differenceBetweenTwoDates,
+    round
 }
--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,10 @@
-import { differenceBetweenTwoDates } from '../index.js';
+import { differenceBetweenTwoDates, round } from '../index.js';
 
 const dateA = new Date(2022, 10, 15)
 const dateB = new Date(2022, 10, 16)
 console.log(differenceBetweenTwoDates(dateA, dateB))
+
+const numA = 123.445
+const numB = 1.005
+console.log(round(numA, 1));
+console.log(round(numB, 2));
